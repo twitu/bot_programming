feca4c44042647de6a3fe54b2bd1e7b720e4cd64
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2019-07-24 00:54:11 +0530

    Refactor helper.py
    Add function to calculate next positions
    Add function for pairwise iteration of an iterable

--- helper.py
+++ helper.py
@@ -1,25 +1,31 @@
+import next_moves
+
+from itertools import tee
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
 def add_tuple_elements(a, b):
     return a[0] + b[0], a[1] + b[1]
-
 
 def scale_tuple_elements(a, p):
     return a[0] * p, a[1] * p
 
+def get_next_positions(point, moves):
+    return [add_tuple_elements(point, move) for move in moves]
 
 def get_neighbors(point, map_size):
     m, n = map_size
-    nbors = [(point[0] + 1, point[1] + 1),
-             (point[0] + 1, point[1]),
-             (point[0] + 1, point[1] - 1),
-             (point[0], point[1] + 1),
-             (point[0], point[1] - 1),
-             (point[0] - 1, point[1] + 1),
-             (point[0] - 1, point[1]),
-             (point[0] - 1, point[1] - 1),]
-    i = 0
-    while i != len(nbors):
-        if nbors[i][0] < 0 or nbors[i][0] >= n or nbors[i][1] < 0 or nbors[i][1] >= m:
-            nbors.remove(nbors[i])
-        else:
-            i += 1
-    return nbors
+    octile_moves = next_moves.adjacent_octile()
+    nbors = get_next_positions(point, octile_moves)
+
+    def is_valid(point):
+        x, y = point
+        return not (x < 0 or x >= n or y < 0 or y >= m)
+
+    selected_nbors = [point for point in nbors if is_valid(point)]
+    return selected_nbors
+
