7a4a25364ca9c519c8f03f986827d678c5410373
lsampras <sampras@haptik.co>
2019-08-14 21:52:13 +0530

    pheromone with random walking

---
+++ pheromone-driver.py
@@ -0,0 +1,33 @@
+from pheromone import pheromone
+import numpy as np
+import next_moves
+import random
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+
+def getObstacleData(size,prob):
+    return np.random.choice(a=[False, True], size=(size,size), p=[1-prob, prob])
+
+def addEnemy():
+    x = random.randint(0,19)
+    y = random.randint(0,19)
+    print("Enemy added:" ,x, y)
+    ph.addEnemy(x,y)
+
+obstacles = getObstacleData(20,0.1)
+ph = pheromone(obstacles,next_moves.adjacent_octile(),decayRate=0,dropOff=0.0)
+
+for i in range(40):
+        addEnemy()
+
+# make sure directory exists
+
+for turn in range(10):
+    if(turn%1 == 0):
+        # print("\n\n\n")
+        # print(ph.map)
+        addEnemy()
+        ph.save_map("pheromone-sample/{}".format(turn))
+    ph.propogate()
+    
+
+ph.save_map("pheromone-sample/{}".format(turn))
---
+++ pheromone.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import matplotlib as mpl
+class pheromone(object):
+    def __init__(self,obstacleData,possible_moves,decayRate = 0.7,dropOff = 0.1):
+        # initialize map size of obstacles and set obstacles position as -1
+        self.map = np.zeros(shape = obstacleData.shape, dtype = np.float32) - obstacleData
+        self.obstacleData = obstacleData
+        self.dropOff = dropOff
+        self.moves = possible_moves
+        self.moves.append((0,0))
+        self.multiplier = (1 - decayRate) #normalize
+        self.temp = np.zeros(shape = self.map.shape,dtype = np.float32)
+        self.cmap = mpl.cm.Reds
+        self.cmap.set_under((0.,0.,1.))
+        self.norm = mpl.colors.Normalize(vmin=0, vmax=1)
+        return
+
+    def addEnemy(self,x,y):
+        self.map[x][y] = 1.0
+        
+    def propogate(self):
+        for i,rows in enumerate(self.map):
+            for j,value in enumerate(rows):
+                if value > self.dropOff:
+                    self.propogate_point(value,i,j)
+                
+        self.map = self.temp - self.obstacleData
+        self.temp = np.zeros(shape = self.map.shape,dtype = float)
+
+
+    def propogate_point(self,value,x,y):
+        delta = 0
+        changes = []
+        for move in self.moves:
+            finalx = x + move[0]
+            finaly = y + move[1]
+            if(finalx >= 0 and finalx < self.map.shape[0]):
+                if(finaly >= 0 and finaly < self.map.shape[1]):
+                    if(self.obstacleData[finalx][finaly] == False):
+                        changes.append((finalx,finaly))
+
+        delta = (value * self.multiplier)/len(changes)
+        for pos in changes:
+            self.temp[pos[0]][pos[1]] +=delta
+
+
+    def save_map(self,name):
+        fig, ax = plt.subplots()
+        ax.imshow(self.map, cmap=self.cmap, norm=self.norm)
+        # draw gridlines
+        ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=2)
+        ax.set_xticks(np.arange(-0.5, self.map.shape[0], 1));
+        ax.set_yticks(np.arange(-0.5, self.map.shape[1], 1));
+        plt.savefig(name + ".png")
+        plt.close(fig)
